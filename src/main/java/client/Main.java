30bd86e232258b02a62c5b655bf4c558adc8a0b9
SzymonR <s.rekawek@kainos.com>
2016-03-02 22:42:18 +0100

    val name

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,12 +1,12 @@
 package client;
 
 public class Main  {
-	private static final String CHILD_PACKAGE = "rest.SampleImplementation";
+	private static final String SAMPLE_IMPLEMENTATION_PACKAGE = "rest.SampleImplementation";
 
 	public static void main(String args[]) throws IllegalAccessException, ClassNotFoundException, InstantiationException {
 		CustomClassLoader customClassLoader = new CustomClassLoader();
 
-		Object child = customClassLoader.loadClass(CHILD_PACKAGE).newInstance();
+		Object child = customClassLoader.loadClass(SAMPLE_IMPLEMENTATION_PACKAGE).newInstance();
 
 		System.out.println("Class Loader: " + child.getClass().getClassLoader());
 		System.out.println(child);
